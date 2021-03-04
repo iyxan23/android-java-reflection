3e2d170c8ae338d82c44070151b6cf18d5b04e82
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-04 17:17:13 +0700

    init: Initialize jar-code

---
+++ jar-code/src/main/java/com/iyxan23/jar/example/Main.java
@@ -0,0 +1,16 @@
+package com.iyxan23.jar.example;
+
+public class Main {
+
+    String hello_world;
+    Object wow_did_it_worked;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void cool() {}
+    public String returns_string() {
+        return "";
+    }
+}
